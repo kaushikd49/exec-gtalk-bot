b5ad2f2ea6a88299fe2c00f8bb0928ebbc7c8e5b
kaushik <kaushik@flipkart.com>
2013-09-13 02:26:12 +0530

    accessing msgs now

--- exec-chat.py
+++ exec-chat.py
@@ -22,17 +22,22 @@ class SampleBot(GtalkRobot):
         self.parse_user_auth_config() # update auth config
         print("auth data is ", self.auth_data.keys())
 
-    def command_100_default(self, user, message, args):
-        '''.*?(?s)(?m)'''
+    def listen_to_chat(self, user, message, args):
         try:
             ldap = self.validate_user_and_get_ldap(user.getStripped())
-            custom_message = "hey! watsup %s? I'm a bot" % ldap
+            custom_message = "hey! watsup %s? I'm a bot " % ldap
             self.replyMessage(user, custom_message)
         except ValildationException as excp:
             self.replyMessage(user, excp.message)
-        except Exception as excp:
-            msg =  "Something went wrong %s " % excp.message
-            self.print_and_reply(excp, msg, user)
+        except:
+            msg = "Something went wrong %s " % sys.exc_info()[0]
+            self.print_and_reply(msg, user)
+
+    def command_100_default(self, user, message, args):
+        # '''.*?(?s)(?m)'''
+        '''.*'''
+        print "message insisde ", message
+        self.listen_to_chat(user, message, args)
 
     def parse_user_auth_config(self):
         config_file_path = os.path.join(os.path.dirname(__file__), CONFIG_FILE)
@@ -40,9 +45,9 @@ class SampleBot(GtalkRobot):
         conf = ConfigObj('%s' % config_file_path)
         self.auth_data = conf
 
-    def print_and_reply(self, excp, msg, user):
+    def print_and_reply(self, msg, user):
         print(msg)
-        self.replyMessage(user, excp.message)
+        self.replyMessage(user, msg)
 
     def validate_user_and_get_ldap(self, user_mail_id):
         suffix = '@flipkart.com'

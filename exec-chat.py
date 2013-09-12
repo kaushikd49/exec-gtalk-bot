092c5be5911024b493f46ac0392a4043ba028a08
kaushik <kaushik@flipkart.com>
2013-09-12 22:39:34 +0530

    initial commit

---
+++ exec-chat.py
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/python
+
+import sys
+import time
+import pysftp
+
+
+from PyGtalkRobot import GtalkRobot
+
+class SampleBot(GtalkRobot):
+    
+    def command_100_default(self, user, message, args):
+        '''.*?(?s)(?m)'''
+        custom_message = "hey! watsup? I'm a bot "
+        self.replyMessage(user, custom_message) #time.strftime("%Y-%m-%d %a %H:%M:%S", time.gmtime()))
+
+
+if __name__ == "__main__":
+    bot = SampleBot()
+    bot.setState('available', "Simple Gtalk Robot")
+    bot.start("ks.kshk@gmail.com", "nomorehackkano")

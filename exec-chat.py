969e59c76efeba51a9974e09e4741a4447c702ed
kaushik <kaushik@flipkart.com>
2013-09-13 06:42:14 +0530

    login and exec cmd working :)

--- exec-chat.py
+++ exec-chat.py
@@ -10,8 +10,8 @@ from configobj import ConfigObj
 from PyGtalkRobot import GtalkRobot
 from collections import defaultdict
 
+RUN_ONLY_AT = 'run_only_at'
 REGISTER = 'register'
-
 CONFIG_FILE = 'config/auth_config.ini'
 
 
@@ -26,26 +26,39 @@ class Common:
 class ValildationException(Exception):
     pass
 
+# SSH login exception
+class SSHException(Exception):
+    pass
+
+class SSHCommandExecException(Exception):
+    pass
 
 # Responsible for login and maintaining state
 class SSHLoginPool:
     user_login_dict = defaultdict(dict)
 
-    def login(self, ldap, machines):
+    def ssh(self, ldap, machines):
+        print("machines ", machines)
         for m in machines:
             self.login_and_update(ldap,m)
+        return self.user_login_dict
 
     def login_and_update(self, ldap, host_machine):
         try:
-            if self.is_host_registered(host_machine, ldap):
+            if not self.is_host_registered(host_machine, ldap):
+                print("Attempting logging into %s " % host_machine)
                 con = pysftp.Connection(host_machine)
-                self.user_login_dict[ldap].append(con)
+                print("Logged into %s " % host_machine)
+                self.user_login_dict[ldap][host_machine] = con
         except:
-            Common.log_error()
+            Common.log_error("Error while trying to ssh to %s" % host_machine)
+            raise SSHException()
 
     def is_host_registered(self, host_machine, ldap):
         return ldap in self.user_login_dict and host_machine in self.user_login_dict[ldap]
 
+    def get_pool(self):
+        return self.user_login_dict
 
 # Validate only certain(fk) user and provide ldap.
 class LdapProvider:
@@ -71,29 +84,60 @@ class LdapProvider:
         return ldap
 
 # Parse, validate and exec commands
+# Bug - run_only_at  and register and run
 class CommandParseAndExecutor:
-    def validate_grammar_and_get_res(self, ldap, message):
-        if re.match('%s.*' % REGISTER, message):
-            self.register_machines(ldap, self.get_machines(message))
-        pass
-
-    def get_machines(self, message):
-        array_with_machine_str = re.split(REGISTER,message)[1:]  # ['m1 m2']
-        machine_list = array_with_machine_str[0].strip().split('') # m1, m2
-        if not machine_list:
-            raise ValildationException("Correct way to register machines: % m1 m2", REGISTER)
+    def __init__(self, ssh_pool):
+        self.ssh_provider_pool = ssh_pool
 
     # TODO
     def execute(self, ldap, message):
-        # self.validate_grammar_and_get_res(ldap, message)
-        message = "hey! watsup %s? I'm a bot " % ldap
-        return message
+        result = self.validate_grammar_and_get_res(ldap, message)
+        resp = self.prepare_response(result)
+        # message = "hey! watsup %s? I'm a bot " % ldap
+        return resp
 
+    def validate_grammar_and_get_res(self, ldap, message):
+        if re.match('%s.*' % REGISTER, message):         # register host machines
+            host_machines = self.get_machines(message)
+            self.ssh_provider_pool.ssh(ldap, host_machines)
+        if re.match('%s.*' % RUN_ONLY_AT, message):                    # run cmd on a specific set of hosts # TODO
+            pass
+        else:
+            user_login_dict = self.ssh_provider_pool.get_pool()
+            all_registerd_hosts = user_login_dict[ldap].keys()
+            if not all_registerd_hosts:
+                raise ValildationException("register host(s) before running commands or use %s (m1,m2) cmd " % RUN_ONLY_AT)
+            return self.run_cmd_on_machines(ldap, user_login_dict, all_registerd_hosts, message)
+
+    def get_machines(self, message):
+        print "message is %s " % message
+        array_with_machine_str = re.split(REGISTER,message)[1:]  # ['m1 m2']
+        print("array_with_machine_str %s" % array_with_machine_str)
+        machine_list = array_with_machine_str[0].strip().split(' ') # m1, m2
+        if not machine_list:
+            raise ValildationException("Correct way to register machines: % m1 m2", REGISTER)
+        return machine_list
+
+    def run_cmd_on_machines(self, ldap, user_login_dict, host_machines, command):
+        res = {}
+        try:
+            for host in host_machines:
+                self.ssh_provider_pool.is_host_registered(host,ldap)
+                ssh_conn = user_login_dict[ldap][host]
+                res[host] = ssh_conn.execute(command)
+        except SSHCommandExecException:
+            Common.log_error("Failure while runnign ssh command")
+            raise
+        return res
+
+
+    def prepare_response(self, result_dict):
+        return str(result_dict)
 
 # Central Facade
 class CommandExecBot(GtalkRobot):
     ldap_provider = LdapProvider()
-    command_executor = CommandParseAndExecutor()
+    command_executor = CommandParseAndExecutor(SSHLoginPool())
 
     def command_100_default(self, user, message, args):
         '''.*''' # handle all messages like a boss
@@ -105,7 +149,7 @@ class CommandExecBot(GtalkRobot):
             ldap = self.ldap_provider.get_ldap(user.getStripped())
             response = self.command_executor.execute(ldap, message.strip())
             self.replyMessage(user, response)
-        except ValildationException as e:
+        except (ValildationException, SSHException) as e:
             self.replyMessage(user, e.message)
         except:
             Common.log_error("Exception in listen_to_chat:")
